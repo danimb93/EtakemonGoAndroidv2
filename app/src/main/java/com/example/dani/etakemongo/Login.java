a63d76e43b43ffb8dfdc7e1813a2dc5e8a0873c9
Dani <danimb93@gmail.com>
2017-05-08 18:04:28 +0200

    Initial commit

---
+++ app/src/main/java/com/example/dani/etakemongo/Login.java
@@ -0,0 +1,19 @@
+package com.example.dani.etakemongo;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+    public void abrirRegistrar (View view){
+        Intent i=new Intent(this,Registrar.class);
+        startActivity(i);
+    }
+}
---
+++ app/src/main/java/com/example/dani/etakemongo/Registrar.java
@@ -0,0 +1,50 @@
+package com.example.dani.etakemongo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class Registrar extends AppCompatActivity {
+
+    private EditText nombre,nick,password,email;
+    private Button registrar;
+    private TextView datos;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrar);
+
+        nombre=(EditText)findViewById(R.id.etNombre);
+        nick=(EditText)findViewById(R.id.etNick);
+        password=(EditText)findViewById(R.id.etPassword);
+        email=(EditText)findViewById(R.id.etEmail);
+        registrar=(Button)findViewById(R.id.btnRegistrar);
+        datos=(TextView)findViewById(R.id.tvRespuesta);
+
+
+        registrar.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick (View v){
+                String snombre= nombre.getText().toString();
+                String snick = nick.getText().toString();
+                String spassword=password.getText().toString();
+                String semail= email.getText().toString();
+
+
+                datos.setText(snombre + snick + spassword + semail);
+            }
+        })
+        ;}
+}
+
+
+
+
+
+
+
