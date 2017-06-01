e2a4112d8c65e7894da78de291d567936012a69a
Roberto <roberto.arranz.93@gmail.com>
2017-06-01 22:58:10 +0200

    No acaba de hacer el login pero nose porque.. será tema de conectarme a localhost.. pensaba q lo habia conseguido

--- app/src/main/java/com/example/dani/etakemongo/ProductionFrontends/Login.java
+++ app/src/main/java/com/example/dani/etakemongo/ProductionFrontends/Login.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.example.dani.etakemongo.Modelo.Usuario;
@@ -17,18 +18,20 @@ import com.example.dani.etakemongo.SysTools.RetrofitOwn;
 
 import java.io.Serializable;
 
+import okhttp3.OkHttpClient;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class Login extends AppCompatActivity {
 
     //Variables necesarias de los objetos del layout
     String tag = "Login"; // tag que indica el ciclo de vida de la app
     private EditText email, password;
-    private Button login;
     private Usuario loged;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,11 +42,11 @@ public class Login extends AppCompatActivity {
         //Traemos a las variables los objetos del layout
         email = (EditText) findViewById(R.id.etEmail);
         password = (EditText) findViewById(R.id.etPassword);
-        login = (Button) findViewById(R.id.btnLogin);
-
+        progressBar = (ProgressBar) findViewById(R.id.progressLogin);
+        progressBar.setVisibility(View.GONE);
     }
 
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    /*protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         email = (EditText) findViewById(R.id.etEmail);
         Bundle result = data.getExtras();
@@ -66,7 +69,7 @@ public class Login extends AppCompatActivity {
                Log.d(tag, "E-mail enviado ok");
            }
        }
-    }
+    }*/
 
     @Override
     protected void onStart() {
@@ -109,10 +112,16 @@ public class Login extends AppCompatActivity {
 
     }
 
-    public void doLogin(final View v){
+    public void doLogin(View v){
 
-        RetrofitOwn retrofitOwn = new RetrofitOwn();
-        Retrofit retrofit = retrofitOwn.getObjectRetrofit();
+        progressBar.setVisibility(View.VISIBLE);
+
+        OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+        Retrofit.Builder builder = new Retrofit.Builder()
+                .baseUrl("http://localhost:8080/myapp/")
+                .addConverterFactory(GsonConverterFactory.create());
+
+        Retrofit retrofit = builder.client(httpClient.build()).build();
 
         //Creamos una instancia de retrofit
         GitHubClient login = retrofit.create(GitHubClient.class);
@@ -127,21 +136,22 @@ public class Login extends AppCompatActivity {
             public void onResponse(Call<Usuario> call, Response<Usuario> response) {
                 //Si OK, procedemos a entrar en el mapa
                 if (response.isSuccessful()){
-                    Usuario loged = (Usuario) response.body();
-                    goToMapsActivity(v);
+                    loged = response.body();
                     Log.d(tag, "Logueado correctamente");
                 }
                 else{
                     //Si los datos son erroneos y el user no esta
                     Toast.makeText(Login.this, "Los datos introducidos son incorrectos", Toast.LENGTH_SHORT).show();
                 }
+                progressBar.setVisibility(View.GONE);
             }
 
             @Override
             //Si no hay conexión
             public void onFailure(Call<Usuario> call, Throwable t) {
                 Toast.makeText(Login.this, "No hay conexión", Toast.LENGTH_SHORT).show();
-                Log.d(tag, "ERROR en el loguin");
+                Log.d(tag, "ERROR en el login");
+                progressBar.setVisibility(View.GONE);
             }
         });
     }
