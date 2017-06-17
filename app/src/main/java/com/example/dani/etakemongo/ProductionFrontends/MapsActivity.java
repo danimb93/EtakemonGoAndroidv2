dfff4b7df671d92091745c4489025e1323c95d77
Roberto <roberto.arranz.93@gmail.com>
2017-06-17 15:48:02 +0200

    Circle

--- app/src/main/java/com/example/dani/etakemongo/ProductionFrontends/MapsActivity.java
+++ app/src/main/java/com/example/dani/etakemongo/ProductionFrontends/MapsActivity.java
@@ -3,6 +3,7 @@ package com.example.dani.etakemongo.ProductionFrontends;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -23,6 +24,8 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -34,12 +37,16 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Retrofit;
 
+import static com.example.dani.etakemongo.R.id.map;
+
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     String tag = "MapsActivity";
     private GoogleMap mMap;
     private Marker marcador;
+    private Circle mCircle;
+    private Marker mMarker;
     private GoogleApiClient mGoogleApiClient;
     double lat = 0.0;
     double ing = 0.0;
@@ -78,7 +85,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
+                .findFragmentById(map);
         mapFragment.getMapAsync(this);
 
 
@@ -110,7 +117,31 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
         miUbicacion();
         System.out.println("************************MIRAR AQUI************"+ lat+ing);
+       /* mCircle = mMap.addCircle(new CircleOptions()
+                .center(new LatLng(37.4, -122.1))
+                .radius(1000)
+                .strokeWidth(10)
+                .strokeColor(Color.GREEN)
+                .fillColor(Color.argb(128, 255, 0, 0))
+                .clickable(true));
+*/
 
+        };
+    private void updateMarkerWithCircle(LatLng location) {
+        mCircle.setCenter(location);
+        mMarker.setPosition(location);
+    }
+
+    private void drawMarkerWithCircle(LatLng position){
+        double radiusInMeters = 100.0;
+        int strokeColor = 0xffff0000; //red outline
+        int shadeColor = 0x44ff0000; //opaque red fill
+
+        CircleOptions circleOptions = new CircleOptions().center(position).radius(radiusInMeters).fillColor(shadeColor).strokeColor(strokeColor).strokeWidth(8);
+        mCircle = mMap.addCircle(circleOptions);
+
+        MarkerOptions markerOptions = new MarkerOptions().position(position);
+        mMarker = mMap.addMarker(markerOptions);
     }
 
     //Metodo para incluir un marker, CameraUpdate para centrar la camara a la posicion del marker
@@ -126,6 +157,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .title("Mi posicion")
                 .icon(BitmapDescriptorFactory.fromResource(R.drawable.player8bits)));
         mMap.animateCamera(miUbicacion);
+        /*Circle circle = map.addCircle(new CircleOptions()
+                .center(new LatLng(-33.87365, 151.20689))
+                .radius(10000)
+                .strokeColor(Color.RED)
+                .fillColor(Color.BLUE));*/
         System.out.println("************************MIRAR AQUI************"+ lat+ing);
     }
 
@@ -204,68 +240,6 @@ private void recuperarEtakemons() {
    //call.enqueue(new Callback() {
     }
 
-    private static final double EARTH_RADIUS = 6371000;// meters
-
-    public static double calculateAreaOfGPSPolygonOnEarthInSquareMeters(final List<Location> locations) {
-        return calculateAreaOfGPSPolygonOnSphereInSquareMeters(locations, EARTH_RADIUS);
-    }
-
-    private static double calculateAreaOfGPSPolygonOnSphereInSquareMeters(final List<Location> locations, final double radius) {
-        if (locations.size() < 3) {
-            return 0;
-        }
-
-        final double diameter = radius * 2;
-        final double circumference = diameter * Math.PI;
-        final List<Double> listY = new ArrayList<Double>();
-        final List<Double> listX = new ArrayList<Double>();
-        final List<Double> listArea = new ArrayList<Double>();
-        // calculate segment x and y in degrees for each point
-        final double latitudeRef = locations.get(0).getLatitude();
-        final double longitudeRef = locations.get(0).getLongitude();
-        for (int i = 1; i < locations.size(); i++) {
-            final double latitude = locations.get(i).getLatitude();
-            final double longitude = locations.get(i).getLongitude();
-            listY.add(calculateYSegment(latitudeRef, latitude, circumference));
-            Log.d(LOG_TAG, String.format("Y %s: %s", listY.size() - 1, listY.get(listY.size() - 1)));
-            listX.add(calculateXSegment(longitudeRef, longitude, latitude, circumference));
-            Log.d(LOG_TAG, String.format("X %s: %s", listX.size() - 1, listX.get(listX.size() - 1)));
-        }
-
-        // calculate areas for each triangle segment
-        for (int i = 1; i < listX.size(); i++) {
-            final double x1 = listX.get(i - 1);
-            final double y1 = listY.get(i - 1);
-            final double x2 = listX.get(i);
-            final double y2 = listY.get(i);
-            listArea.add(calculateAreaInSquareMeters(x1, x2, y1, y2));
-            Log.d(LOG_TAG, String.format("area %s: %s", listArea.size() - 1, listArea.get(listArea.size() - 1)));
-        }
-
-        // sum areas of all triangle segments
-        double areasSum = 0;
-        for (final Double area : listArea) {
-            areasSum = areasSum + area;
-        }
-
-        // get abolute value of area, it can't be negative
-        return Math.abs(areasSum);// Math.sqrt(areasSum * areasSum);
-    }
-
-    private static Double calculateAreaInSquareMeters(final double x1, final double x2, final double y1, final double y2) {
-        return (y1 * x2 - x1 * y2) / 2;
-    }
-
-    private static double calculateYSegment(final double latitudeRef, final double latitude, final double circumference) {
-        return (latitude - latitudeRef) * circumference / 360.0;
-    }
-
-    private static double calculateXSegment(final double longitudeRef, final double longitude, final double latitude,
-    final double circumference) {
-        return (longitude - longitudeRef) * circumference * Math.cos(Math.toRadians(latitude)) / 360.0;
-    }
-
-//recuperar listado pokemons
 
 
     @Override
